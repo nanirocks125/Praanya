ca7766aaa30e8ef88633f974e0c91a7cc23a0c38
nanirocks125 <nanirocks125@gmail.com>
2025-08-17 08:54:33 +0530

    Added packages

--- Praanya/Modules/AnalyticsManagement/Tests/AnalyticsManagementTests/AnalyticsManagementTests.swift
+++ Praanya/Modules/AnalyticsManagement/Tests/AnalyticsManagementTests/AnalyticsManagementTests.swift
@@ -1,5 +1,5 @@
 import Testing
-@testable import Features
+@testable import AnalyticsManagement
 
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
--- Praanya/Modules/UserManagement/Package.swift
+++ Praanya/Modules/UserManagement/Package.swift
@@ -4,29 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "Features",
+    name: "UserManagement",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Features",
-            targets: ["Features"]),
-    ],
-    dependencies: [
-        // Add the Core package as a local dependency
-        .package(path: "../Core")
+            name: "UserManagement",
+            targets: ["UserManagement"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Features",
-            dependencies: [
-                // Specify that the target depends on the Core library
-                .product(name: "Core", package: "Core")
-            ]),
+            name: "UserManagement"),
         .testTarget(
-            name: "FeaturesTests",
-            dependencies: ["Features"]
+            name: "UserManagementTests",
+            dependencies: ["UserManagement"]
         ),
     ]
 )
